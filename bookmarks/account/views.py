f98d65eb7312bfeadb8c59f7db4cc32078a57d2e
Omegadefinitiv <118939168+Omegadefinitiv@users.noreply.github.com>
2025-01-22 14:37:12 -0600

    inclusion de login y logout

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,33 +2,33 @@ from django.contrib.auth import authenticate,login
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from bookmarks.account.forms import LoginForm
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
-
-
+@login_required
+def dashboard(request):
+    return render(request,'account/dashboard.html',{'section':'dashboard'})
 
 
 # Create your views here.
 
+#def user_login(request):
+ #   if request.method == 'POST':
+#        form = LoginForm(request.POST)
+ #       if form.is_valid():
+ #           cd = form.cleaned_data
+  #          user = authenticate(request,
+   #                             username=cd['username'],
+    #                            password=cd['password'])
+     #       if user is not None:
+      #          if user.is_active:
+       #             login(request, user)
+        #            return HttpResponse('Autenticacion correcta')
+         #       else:
+        #            return HttpResponse('Esta cuenta esta desabilitada')
+          #  else:
+  #              return HttpResponse('Login invalido intente de nuevo')
+#    else:
+#        form = LoginForm()
+#    return render(request,"account/login.html",{'form':form})
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Autenticacion correcta')
-                else:
-                    return HttpResponse('Esta cuenta esta desabilitada')
-            else:
-                return HttpResponse('Login invalido intente de nuevo')
-    else:
-        form = LoginForm()
-    return render(request,"account/login.html",{'form',form})
-
-    
