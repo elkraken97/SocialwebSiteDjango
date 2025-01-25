e606e1a255bc882726bfd9427063b5cc838d623d
Omegadefinitiv <118939168+Omegadefinitiv@users.noreply.github.com>
2025-01-24 22:43:09 -0600

    Agregamos cambio de contraseña con emaik

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -7,7 +7,19 @@ urlpatterns =[
     path('logout/',auth_views.LogoutView.as_view(template_name="registration/logout.html"),name='logout'),
     path('',views.dashboard,name='dashboard'),
     path('password_change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
-    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done')
+    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-reset/',
+          auth_views.PasswordResetView.as_view(),
+          name='password_reset'),
+    path('password-reset/done/',
+          auth_views.PasswordResetDoneView.as_view(),
+          name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+          auth_views.PasswordResetConfirmView.as_view(),
+          name='password_reset_confirm'),
+    path('password-reset/complete/',
+          auth_views.PasswordResetCompleteView.as_view(),
+          name='password_reset_complete'),
     ]
 
 
